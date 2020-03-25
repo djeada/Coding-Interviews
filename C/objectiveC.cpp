cf5f15c2c9a7d579bd0473f5b1d2592fdbc271a6
djeada <addjellouli1@gmail.com>
2020-03-25 11:35:24 +0100

    New

---
+++ C/objectiveC.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+struct X {
+	int x;
+	void (*fp)(struct X *);
+};
+
+void func1(struct X *xobj) { printf("%s, %d\n", __FUNCTION__, xobj->x); }
+void func2(struct X *xobj) { printf("%s, %d\n", __FUNCTION__, xobj->x); }
+
+int main() {
+	struct X obj;
+	obj.x  = 10;
+	obj.fp = func1;
+	obj.fp(&obj);
+	obj.x  = 20;
+	obj.fp = func2;
+	obj.fp(&obj);
+	
+	return 1;
+}
