24a062852be13af8936adb85aa890848bd87ea99
djeada <addjellouli1@gmail.com>
2021-04-24 18:50:57 +0200

    wip: todo correct combinations

--- src/2_Strings/combinations.cpp
+++ src/2_Strings/combinations.cpp
@@ -1,60 +1,43 @@
 #include <bits/stdc++.h>
-#include <iostream>
-#include <stdio.h>
+#include <set>
 #include <string>
-using namespace std;
 
-static void combination(const string &str, int index, int number,
-                        stack<char> &result);
-template <typename T> void print(const std::stack<T> &stk) {
-  struct cheat : std::stack<T> {
-    using std::stack<T>::c;
-  };
-  const auto &seq = static_cast<const cheat &>(stk).c;
-
-  for (const auto &v : seq)
-    std::cout << v << ' ';
-  std::cout << '\n';
-}
-
-void combination_solution1(const string &str) {
-  stack<char> result;
-  for (int i = 1; i <= str.length(); ++i) {
-    combination(str, 0, i, result);
-  }
-}
-
-static void combination(const string &str, int index, int number,
-                        stack<char> &result) {
+void _combination(const std::string &str, int index, int number,
+                  std::stack<char> &result, std::set<std::string> &results) {
+  
   if (number == 0) {
-    print(result);
+    std::string reverse;
+    auto copy = result;
+    while (!copy.empty()) {
+      reverse.push_back(copy.top());
+      copy.pop();
+    }
+    results.insert(reverse);
     return;
   }
 
-  if (index == str.length()) {
+  if (index >= str.size())
     return;
-  }
 
-  // select the character str[index]
   result.push(str.at(index));
-  combination(str, index + 1, number - 1, result);
+  _combination(str, index + 1, number - 1, result, results);
   result.pop();
 
-  // ignore the character str[index]
-  combination(str, index + 1, number, result);
+  _combination(str, index + 1, number, result, results);
 }
 
-// ==================== solution 2 ====================
-template <long unsigned int N> void print(const string &str, bitset<N> &bits) {
-  for (int i = 0; i < str.length(); ++i) {
-    if (bits[i])
-      cout << str.at(i);
+std::set<std::string> combination1(const std::string &str) {
+  std::set<std::string> results;
+  std::stack<char> result;
+
+  for (int i = 1; i <= str.length(); ++i) {
+    _combination(str, 0, i, result, results);
   }
-  cout << endl;
+
+  return results;
 }
 
-template <long unsigned int N>
-static bool increment(bitset<N> &bits, int length) {
+template <size_t N> static bool increment(std::bitset<N> &bits, int length) {
   int index = length - 1;
 
   while (index >= 0 && bits[index] > 0) {
@@ -69,30 +52,62 @@ static bool increment(bitset<N> &bits, int length) {
   return true;
 }
 
-static void combination_solution2(const string &str) {
-  bitset<100> bits;
+std::set<std::string> combination2(const std::string &str) {
+  std::set<std::string> results;
+  std::bitset<100> bits;
+
   while (increment(bits, str.length())) {
-    print(str, bits);
+    std::string result;
+    for (int i = 0; i < str.length(); ++i) {
+      if (bits[i])
+        result += str.at(i);
+    }
+    results.insert(result);
   }
+
+  return results;
 }
 
-// ==================== test code ====================
-void test(const string &str) {
-  cout << "Test Begins for " << str;
+void test1() {
 
-  cout << "\nSolution 1 \n";
-  combination_solution1(str);
+  std::string str = "a";
+  std::set<std::string> result = {"a"};
+  assert(combination1(str) == result);
+  assert(combination2(str) == result);
+}
 
-  cout << "\nSolution 2 \n";
-  combination_solution2(str);
+void test2() {
+
+  std::string str = "ab";
+  std::set<std::string> result = {"b", "a", "ab"};
+  //assert(identical(combination1(str), result));
+  assert(combination2(str) == result);
+}
+
+void test3() {
+
+  std::string str = "abc";
+  std::set<std::string> result = {"c", "b", "bc", "a", "ac", "ab", "abc"};
+  //assert(combination1(str) == result);
+  assert(combination2(str) == result);
+}
+
+void test4() {
+
+  std::string str = "abcd";
+  std::set<std::string> result = {"d",   "c",   "cd",  "b",   "bd",
+                                  "bc",  "bcd", "a",   "ad",  "ac",
+                                  "acd", "ab",  "abd", "abc", "abcd"};
+  //assert(combination1(str) == result);
+  assert(combination2(str) == result);
 }
 
 int main() {
-  test("");
-  test("a");
-  test("ab");
-  test("abc");
-  test("abcd");
+
+  test1();
+  test2();
+  test3();
+  test4();
 
   return 0;
 }
