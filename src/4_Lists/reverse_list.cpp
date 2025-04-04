30a6da37abf51135068c0768ad58893606e30bbf
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2025-04-04 20:34:51 +0000

    Update reverse_list.cpp

--- src/4_Lists/reverse_list.cpp
+++ src/4_Lists/reverse_list.cpp
@@ -1,34 +1,69 @@
-#include "list.h"
 #include <cassert>
+#include <iostream>
+#include <memory>
+
+class List {
+public:
+  struct Node {
+    int data;
+    std::unique_ptr<Node> next;
+    Node(int d) : data(d), next(nullptr) {}
+  };
+
+  std::unique_ptr<Node> head;
+
+  List() : head(nullptr) {}
+
+  // Append a new node with the given data.
+  void append(int data) {
+    auto newNode = std::make_unique<Node>(data);
+    if (!head) {
+      head = std::move(newNode);
+    } else {
+      Node* curr = head.get();
+      while (curr->next)
+        curr = curr->next.get();
+      curr->next = std::move(newNode);
+    }
+  }
+
+  // Equality operator compares lists node by node.
+  friend bool operator==(const List &l1, const List &l2) {
+    const Node* n1 = l1.head.get();
+    const Node* n2 = l2.head.get();
+    while (n1 && n2) {
+      if (n1->data != n2->data)
+        return false;
+      n1 = n1->next.get();
+      n2 = n2->next.get();
+    }
+    return n1 == n2;
+  }
+};
 
 class ListWithReversion : public List {
 public:
   ListWithReversion() : List() {}
 
+  // Reverse the list by re-linking the unique_ptr nodes.
   void reverse() {
-    std::shared_ptr<Node> reversedHead = nullptr;
-    std::shared_ptr<Node> prev = nullptr;
-    auto node = head;
-    while (node) {
-      auto next = node->next;
-
-      if (!next) {
-        reversedHead = node;
-      }
-
-      node->next = prev;
-      prev = node;
-      node = next;
+    std::unique_ptr<Node> new_head = nullptr;
+    // While there are nodes in the original list:
+    while (head) {
+      // Detach the first node from the list.
+      auto temp = std::move(head->next);
+      // Insert it at the front of the new list.
+      head->next = std::move(new_head);
+      new_head = std::move(head);
+      // Continue with the rest of the original list.
+      head = std::move(temp);
     }
-
-    head = reversedHead;
+    head = std::move(new_head);
   }
 };
 
 void test1() {
-
   ListWithReversion list;
-
   list.append(1);
   list.append(2);
   list.append(3);
@@ -36,7 +71,6 @@ void test1() {
   list.append(5);
 
   List expected;
-
   expected.append(5);
   expected.append(4);
   expected.append(3);
@@ -44,29 +78,23 @@ void test1() {
   expected.append(1);
 
   list.reverse();
-
   assert(list == expected);
 }
 
 void test2() {
   ListWithReversion list;
   list.append(1);
-
   List expected;
   expected.append(1);
 
   list.reverse();
-
   assert(list == expected);
 }
 
 void test3() {
   ListWithReversion list;
-
   List expected;
-
   list.reverse();
-
   assert(list == expected);
 }
 
@@ -75,5 +103,6 @@ int main() {
   test2();
   test3();
 
+  std::cout << "All tests passed!\n";
   return 0;
 }
