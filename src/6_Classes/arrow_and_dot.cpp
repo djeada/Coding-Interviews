79b8ed46b1f621afcec3de403d70df5ca17d900b
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2025-04-05 14:20:25 +0000

    Update arrow_and_dot.cpp

--- src/6_Classes/arrow_and_dot.cpp
+++ src/6_Classes/arrow_and_dot.cpp
@@ -1,61 +1,89 @@
+/*
+ * Arrow and Dot Showcase
+ *
+ * This program demonstrates how to access class members using both the dot (.) and
+ * arrow (->) operators in C++. A class "Human" is defined with private and public
+ * members. We create an object (accessed via the dot operator) and a pointer to an object
+ * (accessed via the arrow operator). The program also uses the scope resolution operator (::)
+ * to call a static member function.
+ *
+ * ASCII Illustration:
+ *
+ *       +--------------+
+ *       |    Human     |
+ *       +--------------+
+ *       | - age        |
+ *       | - occupation |
+ *       | + name       |
+ *       +--------------+
+ *             |
+ *   +---------+---------+
+ *   |                   |
+ * Object             Pointer
+ * (dot operator)     (arrow operator)
+ *
+ * Example Output:
+ * Name: Jim
+ * Jim of age 13 is doing something.
+ * Jim works as a Student.
+ * Name: Tom
+ * Tom of age 24 is doing something.
+ * Tom works as a Engineer.
+ * Tom works as a Data Scientist.
+ * Jim is 13 years old.
+ */
+
 #include <iostream>
 #include <string>
 
 class Human {
 private:
-  int age;
-  std::string occupation;
+    int age;
+    std::string occupation;
 
 public:
-  std::string name;
+    std::string name;
 
-  // Constructor to initialize a Human object
-  Human(int humanAge, const std::string &humanName,
-        const std::string &humanOccupation)
-      : age(humanAge), name(humanName), occupation(humanOccupation) {}
+    Human(int humanAge, const std::string &humanName, const std::string &humanOccupation)
+        : age(humanAge), name(humanName), occupation(humanOccupation) {}
 
-  // Member function to simulate an action
-  void DoSomething() const {
-    std::cout << name << " of age " << age << " is doing something.\n";
-  }
+    void DoSomething() const {
+        std::cout << name << " of age " << age << " is doing something.\n";
+    }
 
-  // Member function to set occupation
-  void SetOccupation(const std::string &newOccupation) {
-    occupation = newOccupation;
-  }
+    void SetOccupation(const std::string &newOccupation) {
+        occupation = newOccupation;
+    }
 
-  // Member function to display occupation
-  void DisplayOccupation() const {
-    std::cout << name << " works as a " << occupation << ".\n";
-  }
+    void DisplayOccupation() const {
+        std::cout << name << " works as a " << occupation << ".\n";
+    }
 
-  // Static function to display age of a Human object
-  static void DisplayAge(const Human &person) {
-    std::cout << person.name << " is " << person.age << " years old.\n";
-  }
+    static void DisplayAge(const Human &person) {
+        std::cout << person.name << " is " << person.age << " years old.\n";
+    }
 };
 
 int main() {
-  // Usage of Dot(.)
-  Human firstMan(13, "Jim", "Student");
-  std::cout << "Name: " << firstMan.name << std::endl;
-  firstMan.DoSomething();
-  firstMan.DisplayOccupation();
+    // Using the dot operator on an object
+    Human firstMan(13, "Jim", "Student");
+    std::cout << "Name: " << firstMan.name << "\n";
+    firstMan.DoSomething();
+    firstMan.DisplayOccupation();
 
-  // Usage of Pointer Operator (->)
-  Human *secondMan = new Human(24, "Tom", "Engineer");
-  std::cout << "Name: " << secondMan->name << std::endl;
-  secondMan->DoSomething();
-  secondMan->DisplayOccupation();
+    // Using the arrow operator on a pointer to an object
+    Human *secondMan = new Human(24, "Tom", "Engineer");
+    std::cout << "Name: " << secondMan->name << "\n";
+    secondMan->DoSomething();
+    secondMan->DisplayOccupation();
 
-  // Change occupation
-  secondMan->SetOccupation("Data Scientist");
-  secondMan->DisplayOccupation();
+    // Changing occupation using the pointer
+    secondMan->SetOccupation("Data Scientist");
+    secondMan->DisplayOccupation();
 
-  // Usage of Double Colon (::)
-  Human::DisplayAge(firstMan);
+    // Using the scope resolution operator to call a static member function
+    Human::DisplayAge(firstMan);
 
-  delete secondMan;
-
-  return 0;
+    delete secondMan;
+    return 0;
 }
